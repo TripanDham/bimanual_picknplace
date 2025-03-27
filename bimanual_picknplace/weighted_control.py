b580d45d528cfb106f44ab7b77f9808c7b39d820
aimanbayezeed <aiman.bayezeed@gmail.com>
2025-03-27 06:30:34 -0700

    weights and jacobian

--- bimanual_picknplace/weighted_control.py
+++ bimanual_picknplace/weighted_control.py
@@ -13,6 +13,9 @@ import tf_transformations
 import hello_helpers.hello_misc as hm
 import os
 import time
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from control_msgs.action import FollowJointTrajectory
+from rclpy.duration import Duration
 
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = '/usr/lib/x86_64-linux-gnu/qt5/plugins/platforms/libqxcb.so'
 
@@ -22,35 +25,81 @@ class WeightedController(hm.HelloNode):
         hm.HelloNode.main(self, 'weighted_controller','weighted_controller',wait_for_first_pointcloud=False)
         
         self.callback_group = ReentrantCallbackGroup()
-        self.base_pub = self.create_publisher(Twist, '/b/stretch/cmd_vel', 10, callback_group=self.callback_group)
-        
-        self.global_localisation = 1  #1: transform from odom to grasp center used, 0: transform from b_odom used
+        self.base_pub = self.create_publisher(Twist, '/stretch/cmd_vel', 10, callback_group=self.callback_group)
+
+        self.odom_sub = self.create_subscription(Odometry, '/b/odom', self.odom_callback, 1, callback_group=self.callback_group)
+        self.tube_vel_sub = self.create_subscription(Twist, '/b/velocity', self.vel_cb, 1, callback_group=self.callback_group)
 
         time.sleep(2)
-        self.localize()
 
-    def localize(self):
-        global_transform = TransformStamped()
-        if self.global_localisation == 1:
-            parent_frame = 'odom'
-        else:
-            parent_frame = 'b_odom'
+    def odom_callback(self, odom_msg):
+        orientation = odom_msg.pose.pose.orientation
+        self.tb = tf_transformations.euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])[2]
         
-        try:
-            global_transform = self.tf2_buffer.lookup_transform('b_link_grasp_center', parent_frame, rclpy.time.Time())
-        except:
-            self.get_logger().info("Could not recieve TF")
+        self.tr = self.joint_state['b_joint_wrist_roll']
+        self.ty = self.joint_state['b_joint_wrist_yaw']
+        self.tp = self.joint_state['b_joint_wrist_pitch']
+        self.da = self.joint_state['b_joint_arm_l0'] * 4
+        self.x_base = odom_msg.pose.pose.position.x
+        self.y_base = odom_msg.pose.pose.position.y
+        self.dl = self.joint_state['b_joint_lift']
 
-        x = global_transform.transform.translation.x
-        y = global_transform.transform.translation.y
-        z = global_transform.transform.translation.z
+    def weighted_control(self, v_ee):
+        tb = self.tb
+        tr = self.tr
+        ty = self.ty
+        tp = self.tp
+        da = self.da
+        j = np.array([[np.cos(tb), 0.1687*np.cos(tb) + 0.017*np.sin(tb) - 0.251*np.cos(tr)*np.cos(tb + ty) - 1.0*np.cos(tb)*da - 0.251*np.cos(tp)*np.sin(tr)*np.sin(tb + ty),   0, -1.0*np.sin(tb), - 0.251*np.cos(tr)*np.cos(tb + ty) - 0.251*np.cos(tp)*np.sin(tr)*np.sin(tb + ty), -0.251*np.sin(tp)*np.sin(tr)*np.cos(tb + ty), 0.251*np.sin(tr)*np.sin(tb + ty) + 0.251*np.cos(tp)*np.cos(tr)*np.cos(tb + ty)],
+                      [np.sin(tb), 0.1687*np.sin(tb) - 0.017*np.cos(tb) - 0.251*np.cos(tr)*np.sin(tb + ty) - 1.0*np.sin(tb)*da + 0.251*np.cos(tp)*np.sin(tr)*np.cos(tb + ty),   0,      np.cos(tb),   0.251*np.cos(tp)*np.sin(tr)*np.cos(tb + ty) - 0.251*np.cos(tr)*np.sin(tb + ty), -0.251*np.sin(tp)*np.sin(tr)*np.sin(tb + ty), 0.251*np.cos(tp)*np.cos(tr)*np.sin(tb + ty) - 0.251*np.sin(tr)*np.cos(tb + ty)],
+                      [0,0, 1.0, 0,  0,  0.251*np.cos(tp)*np.sin(tr), 0.251*np.cos(tr)*np.sin(tp)],
+                      [0,0,  0,  0,0, np.sin(tb(1) + ty(1)),np.cos(tb(1) + ty(1))*np.sin(tp(1))],
+                      [0, 0, 0, 0,0,-1.0*np.cos(tb(1) + ty(1)),np.sin(tb(1) + ty(1))*np.sin(tp(1))],
+                      [0, 1.0,   0,0,1.0,0,-1.0*np.cos(tp(1))]])
         
-        orientation = global_transform.transform.rotation
-        quat = [orientation.x, orientation.y, orientation.z, orientation.w]
-        ax, ay, az = tf_transformations.euler_from_quaternion(quat)
-        
-        self.pose = [x,y,z,ax,ay,az]
+        w_arm = 1
+        w_base = 1
+        w_wrist = 1
 
-        return self.pose
+        w = np.diag([(w_arm + w_base)/2, (w_arm + w_base)/2, w_arm, w_arm, w_wrist, w_wrist, w_wrist])
 
-        
+        j_inv = w @ np.transpose(j) @ np.linalg.inv(j @ w @ np.transpose(j))
+        v_robot = j_inv @ v_ee
+
+        return v_robot
+    
+    def vel_cb(self, vel):
+        vx = vel.linear.x
+        vy = vel.linear.y
+        vz = vel.linear.z
+
+        wx = vel.angular.x
+        wy = vel.angular.y
+        wz = vel.angular.z
+
+        vel = np.array([vx, vy, vz, wx, wy, wz])
+
+        v_robot = self.weighted_control(vel)
+        self.send_vel(v_robot)
+
+    def send_vel(self, v_robot):
+        robot_joints = ['joint_lift', 'joint_arm_l0', 'joint_arm_l1', 'joint_arm_l2', 'joint_arm_l3', 'joint_wrist_yaw', 'joint_wrist_pitch', 'joint_wrist_roll']
+        robot_velocities = [v_robot[2], v_robot[3], v_robot[3], v_robot[3], v_robot[3], v_robot[4], v_robot[5], v_robot[6]]
+
+        point = JointTrajectoryPoint()
+        point.time_from_start = Duration(seconds=0.0).to_msg()
+
+        point.velocities = robot_velocities
+        joint_names = robot_joints
+
+        trajectory_goal = FollowJointTrajectory.Goal()
+        trajectory_goal.goal_time_tolerance = Duration(seconds=1.0).to_msg()
+        trajectory_goal.trajectory.joint_names = joint_names
+        trajectory_goal.trajectory.points = [point]
+        self.trajectory_client.send_goal_async(trajectory_goal)
+
+        v_base = Twist()
+
+        v_base.linear.x = v_robot[0]
+        v_base.angular.z = v_robot[1]
+        self.base_pub.publish()
